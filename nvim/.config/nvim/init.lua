24062bbbed218f877f0f367e0817f269685b282f
Jesús Datica <jesusado.89@gmail.com>
2024-08-23 22:51:04 -0400

    actualizando

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,7 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
 vim.o.relativenumber = true
+vim.o.shell = "/usr/bin/fish"
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -5,7 +5,7 @@
 local M = {}
 
 M.ui = {
-  theme = "chadracula-evondev",
+  theme = "tokyonight",
   transparency = true,
 
   -- hl_override = {
