093ca05a97ef6a2e4ac19d6b82c45126d9871242
Jesús Datica <jesusado.89@gmail.com>
2024-09-29 22:37:39 -0400

    actualizando

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -2,7 +2,36 @@
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
-local M = {}
+local M = {
+  mason = {
+    cmd = true,
+    pkgs = {
+      "lua-language-server",
+      "stylua",
+      "html-lsp",
+      "css-lsp",
+      "typescript-language-server",
+      "prettier",
+      "pyright",
+      "intelephense",
+      "black",
+      "mypy",
+      "ruff",
+      "debugpy",
+      "bash-language-server",
+      "shfmt",
+      "dockerfile-language-server",
+      "docker-compose-language-service",
+      "yaml-language-server",
+      "sqlls",
+      "phpcbf",
+      "eslint_d",
+      "gopls",
+      "json-lsp",
+      "vtsls",
+    },
+  },
+}
 
 M.ui = {
   theme = "chadracula-evondev",
--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -21,7 +21,14 @@ for _, lsp in ipairs(servers) do
 end
 
 -- typescript
-lspconfig.tsserver.setup {
+lspconfig.ts_ls.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
+
+-- vtsls
+lspconfig.vtsls.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
