0b0e3bd62bc2204b75d105087b8267c2601a4b89
Jesus <jesusado.89@gmail.com>
2025-07-23 15:16:33 -0400

    add block

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -109,54 +109,62 @@ c.tabs.title.format = "{audio}{current_title}"
 c.fonts.web.size.default = 20
 
 c.url.searchengines = {
-# note - if you use duckduckgo, you can make use of its built in bangs, of which there are many! https://duckduckgo.com/bangs
-        'DEFAULT': 'https://duckduckgo.com/?q={}',
-        '!aw': 'https://wiki.archlinux.org/?search={}',
-        '!apkg': 'https://archlinux.org/packages/?sort=&q={}&maintainer=&flagged=',
-        '!gh': 'https://github.com/search?o=desc&q={}&s=stars',
-        '!yt': 'https://www.youtube.com/results?search_query={}',
-        }
+    # note - if you use duckduckgo, you can make use of its built in bangs, of which there are many! https://duckduckgo.com/bangs
+    "DEFAULT": "https://duckduckgo.com/?q={}",
+    "!aw": "https://wiki.archlinux.org/?search={}",
+    "!apkg": "https://archlinux.org/packages/?sort=&q={}&maintainer=&flagged=",
+    "!gh": "https://github.com/search?o=desc&q={}&s=stars",
+    "!yt": "https://www.youtube.com/results?search_query={}",
+}
 
-c.completion.open_categories = ['searchengines', 'quickmarks', 'bookmarks', 'history', 'filesystem']
+c.completion.open_categories = [
+    "searchengines",
+    "quickmarks",
+    "bookmarks",
+    "history",
+    "filesystem",
+]
 
-config.load_autoconfig() # load settings done via the gui
+config.load_autoconfig()  # load settings done via the gui
 
-c.auto_save.session = False # save tabs on quit/restart
+c.auto_save.session = False  # save tabs on quit/restart
 c.completion.web_history.max_items = 0
 
 # keybinding changes
-config.bind('=', 'cmd-set-text -s :open')
-config.bind('h', 'history')
-config.bind('cs', 'cmd-set-text -s :config-source')
-config.bind('tH', 'config-cycle tabs.show multiple never')
-config.bind('sH', 'config-cycle statusbar.show always never')
-config.bind('T', 'hint links tab')
-config.bind('pP', 'open -- {primary}')
-config.bind('pp', 'open -- {clipboard}')
-config.bind('pt', 'open -t -- {clipboard}')
-config.bind('qm', 'macro-record')
-config.bind('<ctrl-y>', 'spawn --userscript ytdl.sh')
-config.bind('tT', 'config-cycle tabs.position top left')
-config.bind('gJ', 'tab-move +')
-config.bind('gK', 'tab-move -')
-config.bind('gm', 'tab-move')
+config.bind("=", "cmd-set-text -s :open")
+config.bind("h", "history")
+config.bind("cs", "cmd-set-text -s :config-source")
+config.bind("tH", "config-cycle tabs.show multiple never")
+config.bind("sH", "config-cycle statusbar.show always never")
+config.bind("T", "hint links tab")
+config.bind("pP", "open -- {primary}")
+config.bind("pp", "open -- {clipboard}")
+config.bind("pt", "open -t -- {clipboard}")
+config.bind("qm", "macro-record")
+config.bind("<ctrl-y>", "spawn --userscript ytdl.sh")
+config.bind("tT", "config-cycle tabs.position top left")
+config.bind("gJ", "tab-move +")
+config.bind("gK", "tab-move -")
+config.bind("gm", "tab-move")
 
 # dark mode setup
 c.colors.webpage.darkmode.enabled = True
-c.colors.webpage.darkmode.algorithm = 'lightness-cielab'
-c.colors.webpage.darkmode.policy.images = 'never'
-config.set('colors.webpage.darkmode.enabled', False, 'file://*')
+c.colors.webpage.darkmode.algorithm = "lightness-cielab"
+c.colors.webpage.darkmode.policy.images = "never"
+config.set("colors.webpage.darkmode.enabled", False, "file://*")
 
 # styles, cosmetics
 # c.content.user_stylesheets = ["~/.config/qutebrowser/styles/youtube-tweaks.css"]
-c.tabs.padding = {'top': 5, 'bottom': 5, 'left': 9, 'right': 9}
-c.tabs.indicator.width = 0 # no tab indicators
+c.tabs.padding = {"top": 5, "bottom": 5, "left": 9, "right": 9}
+c.tabs.indicator.width = 0  # no tab indicators
 # c.window.transparent = True # apparently not needed
-c.tabs.width = '7%'
+c.tabs.width = "7%"
 
 # fonts
 c.fonts.default_family = "FiraCode Nerd Font"
-c.fonts.default_size = '13pt'  # Dejamos el tamaño que ya tenías, ¡puedes cambiarlo si quieres!
+c.fonts.default_size = (
+    "13pt"  # Dejamos el tamaño que ya tenías, ¡puedes cambiarlo si quieres!
+)
 c.fonts.web.family.fixed = "FiraCode Nerd Font"
 c.fonts.web.family.sans_serif = "FiraCode Nerd Font"
 c.fonts.web.family.serif = "FiraCode Nerd Font"
@@ -175,23 +183,29 @@ config.set("content.cookies.store", True)
 
 # Adblocking info -->
 c.content.blocking.enabled = True
-c.content.blocking.method = 'adblock' # uncomment this if you install python-adblock
+c.content.blocking.method = "adblock"  # uncomment this if you install python-adblock
 c.content.blocking.adblock.lists = [
-        "https://github.com/ewpratten/youtube_ad_blocklist/blob/master/blocklist.txt",
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt",
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt",
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt",
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2021.txt",
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2022.txt",
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2023.txt",
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2024.txt",
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt",
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt",
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-cookies.txt",
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-others.txt",
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/quick-fixes.txt",
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt"]
+    "https://easylist.to/easylist/easylist.txt",
+    "https://easylist.to/easylist/easyprivacy.txt",
+    "https://secure.fanboy.co.nz/fanboy-annoyance.txt",
+    "https://secure.fanboy.co.nz/fanboy-social.txt",
+    #############################################################################################
+    # "https://github.com/ewpratten/youtube_ad_blocklist/blob/master/blocklist.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2021.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2022.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2023.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2024.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-cookies.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-others.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/quick-fixes.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt",
+]
